ff7b2bd3840277a737caa61242c10adf1cf40dff
Emil Tin <emil@tin.dk>
2019-12-18 16:17:57 +0100

    refactor logging in command spec

--- spec/site/command_spec.rb
+++ spec/site/command_spec.rb
@@ -1,5 +1,16 @@
-def unsubscribe_from_all task,supervisor,site
-  message, response = site.unsubscribe_to_status @component, [
+# Test command requests by sending commands and checking 
+# responses and status updates
+
+def log_confirmation action, &block
+  @site.log "Waiting for confirmation of #{action}", level: :test
+  start_time = Time.now
+  yield block
+  delay = Time.now - start_time
+  @site.log "#{action.capitalize} confirmed after #{delay.to_i}s", level: :test
+end
+
+def unsubscribe_from_all
+  message, response = @site.unsubscribe_to_status @component, [
     {'sCI'=>'S0014','n'=>'status'},
     {'sCI'=>'S0011','n'=>'status'},
     {'sCI'=>'S0007','n'=>'status'},
@@ -11,69 +22,64 @@ def unsubscribe_from_all task,supervisor,site
   ]
 end
 
-def subscribe task,supervisor,site
-  message, response = site.subscribe_to_status @component, [{'sCI'=>@status_code_id,'n'=>@status_name,'uRt'=>'1'}], 180
+def subscribe
+  message, response = @site.subscribe_to_status @component, [{'sCI'=>@status_code_id,'n'=>@status_name,'uRt'=>'1'}], 180
 end
 
-def set_plan task,supervisor,site, plan
+def set_plan plan
   status = 'True'
   securityCode = SECRETS['security_codes'][2]
 
-  site.log "Switching to plan #{plan}", level: :test
+  @site.log "Switching to plan #{plan}", level: :test
   start_time = Time.now
-  site.send_command @component, [
+  @site.send_command @component, [
     {'cCI' => @command_code_id, 'cO' => @command_name, 'n' => 'status', 'v' => status},
     {'cCI' => @command_code_id, 'cO' => @command_name, 'n' => 'securityCode', 'v' => securityCode},
     {'cCI' => @command_code_id, 'cO' => @command_name, 'n' => 'timeplan', 'v' => plan}
   ]
 
-  site.log "Waiting for command response confirming intention to switch to plan #{plan}", level: :test
-  response = site.wait_for_command_response component: @component, timeout: 180
+  log_confirmation"intention to switch to plan #{plan}" do
+    response = @site.wait_for_command_response component: @component, timeout: 180
 
-  expect(response).to be_a(RSMP::CommandResponse)
-  expect(response.attributes['cId']).to eq(@component)
+    expect(response).to be_a(RSMP::CommandResponse)
+    expect(response.attributes['cId']).to eq(@component)
 
-  age = 'recent'
-  expect(response.attributes['rvs']).to eq([
-    { 'cCI' => @command_code_id, 'n' => 'status','v' => status, 'age' => age },
-    { 'cCI' => @command_code_id, 'n' => 'securityCode','v' => securityCode, 'age' => age },
-    { 'cCI' => @command_code_id, 'n' => 'timeplan','v' => plan, 'age' => age }
-  ])
-
-  delay = Time.now - start_time
-  site.log "Intention to switch to plan #{plan} confirmed after #{delay.to_i}s", level: :test
+    age = 'recent'
+    expect(response.attributes['rvs']).to eq([
+      { 'cCI' => @command_code_id, 'n' => 'status','v' => status, 'age' => age },
+      { 'cCI' => @command_code_id, 'n' => 'securityCode','v' => securityCode, 'age' => age },
+      { 'cCI' => @command_code_id, 'n' => 'timeplan','v' => plan, 'age' => age }
+    ])
+  end
 end
 
-def set_functional_position task,supervisor,site,status
+def set_functional_position status
   timeout = '0'
   intersection = '0'
   securityCode = SECRETS['security_codes'][2]
 
-  site.log "Switching to " + status, level: :test
-  start_time = Time.now
-  site.send_command @component, [
+  @site.log "Switching to " + status, level: :test
+  @site.send_command @component, [
     {'cCI' => @command_code_id, 'cO' => @command_name, 'n' => 'status', 'v' => status},
     {'cCI' => @command_code_id, 'cO' => @command_name, 'n' => 'securityCode', 'v' => securityCode},
     {'cCI' => @command_code_id, 'cO' => @command_name, 'n' => 'timeout', 'v' => timeout},
     {'cCI' => @command_code_id, 'cO' => @command_name, 'n' => 'intersection', 'v' => intersection}
   ]
 
-  site.log "Waiting for command response confirming intention to switch to " + status, level: :test
-  response = site.wait_for_command_response component: @component, timeout: 180
+  log_confirmation"intention to switch to  #{status}" do
+    response = @site.wait_for_command_response component: @component, timeout: 180
 
-  expect(response).to be_a(RSMP::CommandResponse)
-  expect(response.attributes['cId']).to eq(@component)
+    expect(response).to be_a(RSMP::CommandResponse)
+    expect(response.attributes['cId']).to eq(@component)
 
-  age = 'recent'
-  expect(response.attributes['rvs']).to eq([
-    { 'cCI' => @command_code_id, 'n' => 'status','v' => status, 'age' => age },
-    { 'cCI' => @command_code_id, 'n' => 'securityCode','v' => securityCode, 'age' => age },
-    { 'cCI' => @command_code_id, 'n' => 'timeout','v' => timeout, 'age' => age },
-    { 'cCI' => @command_code_id, 'n' => 'intersection','v' => intersection, 'age' => age }
-  ])
-
-  delay = Time.now - start_time
-  site.log "Intention to switch to " + status + " confirmed after #{delay.to_i}s", level: :test
+    age = 'recent'
+    expect(response.attributes['rvs']).to eq([
+      { 'cCI' => @command_code_id, 'n' => 'status','v' => status, 'age' => age },
+      { 'cCI' => @command_code_id, 'n' => 'securityCode','v' => securityCode, 'age' => age },
+      { 'cCI' => @command_code_id, 'n' => 'timeout','v' => timeout, 'age' => age },
+      { 'cCI' => @command_code_id, 'n' => 'intersection','v' => intersection, 'age' => age }
+    ])
+  end
 end
 
 
@@ -92,139 +98,132 @@ def get_intersection_boolean boolean,intersections
   status
 end
 
-def switch task,supervisor,site,plan
-  set_plan task,supervisor,site, plan
-  site.log "Waiting for status update that confirms switch to plan #{plan}", level: :test
-  start_time = Time.now
-  response = site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s: plan, timeout: 180
-  current_plan = get_status_value response
-  expect(current_plan).to eq(plan)
-  delay = Time.now - start_time
-  site.log "Switch to plan #{plan} confirmed after #{delay.to_i}s", level: :test
+def switch plan
+  set_plan plan
+  log_confirmation"intention to switch to plan #{plan}" do
+    response = @site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s: plan, timeout: 180
+    current_plan = get_status_value response
+    expect(current_plan).to eq(plan)
+  end
 end
 
-def switch_yellow_flash task,supervisor,site,intersections
-  set_functional_position task,supervisor,site,'YellowFlash'
-  site.log "Waiting for status update that confirms switch to yellow flash", level: :test
-  start_time = Time.now
-
-  # Wait for yellow flash status to be true
-  @status_code_id = 'S0011'
-  @status_name = 'status'
-  subscribe task,supervisor,site
-  status = get_intersection_boolean 'True',intersections
-  response = site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
-  current_functional_position = get_status_value response
-  expect(current_functional_position).to eq(status)
-  unsubscribe_from_all task,supervisor,site
-
-  delay = Time.now - start_time
-  site.log "Switch to yellow flash confirmed after #{delay.to_i}s", level: :test
+def switch_yellow_flash intersections
+  set_functional_position 'YellowFlash'
+  log_confirmation "switch to yellow flash" do
+    # Wait for yellow flash status to be true
+    @status_code_id = 'S0011'
+    @status_name = 'status'
+    subscribe
+    status = get_intersection_boolean 'True',intersections
+    response = @site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
+    current_functional_position = get_status_value response
+    expect(current_functional_position).to eq(status)
+    unsubscribe_from_all
+  end
 end
 
-def switch_dark_mode task,supervisor,site,intersections
+def switch_dark_mode intersections
   set_functional_position task,supervisor,site,'Dark'
-  site.log "Waiting for status update that confirms switch to dark mode", level: :test
-  start_time = Time.now
-
-  @status_code_id = 'S0007'
-  @status_name = 'status'
-  subscribe task,supervisor,site
-  status = get_intersection_boolean 'False',intersections
-  response = site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
-  current_functional_position = get_status_value response
-  expect(current_functional_position).to eq(status)
-  unsubscribe_from_all task,supervisor,site
-
-  delay = Time.now - start_time
-  site.log "Switch to dark mode confirmed after #{delay.to_i}s", level: :test
+  log_confirmation "switch to dark nmode" do
+    @status_code_id = 'S0007'
+    @status_name = 'status'
+    subscribe
+    status = get_intersection_boolean 'False',intersections
+    response = @site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
+    current_functional_position = get_status_value response
+    expect(current_functional_position).to eq(status)
+    unsubscribe_from_all
+  end
 end
 
-def switch_normal_control task,supervisor,site,intersections
+def switch_normal_control intersections
   set_functional_position task,supervisor,site,'NormalControl'
-  site.log "Waiting for status update that confirms switch to NormalControl", level: :test
-  start_time = Time.now
+  log_confirmation "switch to NormalControl" do
+    # Wait for 'switched on' to be true (dark mode false)
+    @status_code_id = 'S0007'
+    @status_name = 'status'
+    subscribe
+    status = get_intersection_boolean 'True',intersections
+    response = @site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
+    current_dark_mode = get_status_value response
+    expect(current_dark_mode).to eq(status)
+    unsubscribe_from_all
 
-  # Wait for 'switched on' to be true (dark mode false)
-  @status_code_id = 'S0007'
-  @status_name = 'status'
-  subscribe task,supervisor,site
-  status = get_intersection_boolean 'True',intersections
-  response = site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
-  current_dark_mode = get_status_value response
-  expect(current_dark_mode).to eq(status)
-  unsubscribe_from_all task,supervisor,site
+    # Wait for yellow flash status to be false
+    @status_code_id = 'S0011'
+    @status_name = 'status'
+    subscribe
+    status = get_intersection_boolean 'False',intersections
+    response = @site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
+    current_dark_mode = get_status_value response
+    expect(current_dark_mode).to eq(status)
+    unsubscribe_from_all
 
-  # Wait for yellow flash status to be false
-  @status_code_id = 'S0011'
-  @status_name = 'status'
-  subscribe task,supervisor,site
-  status = get_intersection_boolean 'False',intersections
-  response = site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
-  current_dark_mode = get_status_value response
-  expect(current_dark_mode).to eq(status)
-  unsubscribe_from_all task,supervisor,site
-
-  # Wait for startup mode to be false
-  @status_code_id = 'S0005'
-  @status_name = 'status'
-  subscribe task,supervisor,site
-  status = 'False'
-  response = site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
-  current_controller_starting = get_status_value response
-  expect(current_controller_starting).to eq(status)
-  unsubscribe_from_all task,supervisor,site
-
-  delay = Time.now - start_time
-  site.log "Switch to NormalControl confirmed after #{delay.to_i}s", level: :test
+    # Wait for startup mode to be false
+    @status_code_id = 'S0005'
+    @status_name = 'status'
+    subscribe
+    status = 'False'
+    response = @site.wait_for_status_update component: @component, sCI: @status_code_id, n: @status_name, q:'recent', s:status, timeout: 180
+    current_controller_starting = get_status_value response
+    expect(current_controller_starting).to eq(status)
+    unsubscribe_from_all
+  end
 end
 
 RSpec.describe 'RSMP site commands' do
   it 'M0001 set yellow flash' do
-    @component = MAIN_COMPONENT
-    @command_code_id = 'M0001'
-    @command_name = 'setValue'
-
-    intersections = SITE_CONFIG['intersections']
-
     TestSite.connected do |task,supervisor,site|
-      unsubscribe_from_all task,supervisor,site
+      @component = MAIN_COMPONENT
+      @command_code_id = 'M0001'
+      @command_name = 'setValue'
 
-      switch_yellow_flash task,supervisor,site,intersections
-      switch_normal_control task,supervisor,site,intersections
+      intersections = SITE_CONFIG['intersections']
+      
+      @task = task
+      @supervisor = supervisor
+      @site = site
+
+      unsubscribe_from_all
+
+      switch_yellow_flash intersections
+      switch_normal_control intersections
     end
   end
 
   it 'M0001 set dark mode' do
-    @component = MAIN_COMPONENT
-    @command_code_id = 'M0001'
-    @command_name = 'setValue'
-
-    intersections = SITE_CONFIG['intersections']
-
     TestSite.connected do |task,supervisor,site|
-      unsubscribe_from_all task,supervisor,site
+      @component = MAIN_COMPONENT
+      @command_code_id = 'M0001'
+      @command_name = 'setValue'
 
-      switch_dark_mode task,supervisor,site,intersections
-      switch_normal_control task,supervisor,site,intersections
+      intersections = SITE_CONFIG['intersections']
+      @task = task
+      @supervisor = supervisor
+      @site = site
+
+      unsubscribe_from_all
+
+      switch_dark_mode intersections
+      switch_normal_control intersections
     end
   end
 
   it 'M0002 set time plan' do
-    @component = MAIN_COMPONENT
-    @command_code_id = 'M0002'
-    @command_name = 'setPlan'
-
-    @status_code_id = 'S0014'
-    @status_name = 'status'
-
-    plans = SITE_CONFIG['plans']
-
     TestSite.connected do |task,supervisor,site|
-      unsubscribe_from_all task,supervisor,site
-      subscribe task,supervisor,site
+      @component = MAIN_COMPONENT
+      @command_code_id = 'M0002'
+      @command_name = 'setPlan'
+
+      @status_code_id = 'S0014'
+      @status_name = 'status'
+
+      plans = SITE_CONFIG['plans']
+      
+      unsubscribe_from_all
+      subscribe
       plans.each do |plan|
-        switch task,supervisor,site,plan
+        switch plan
       end
     end
   end
