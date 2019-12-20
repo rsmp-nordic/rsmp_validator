fbff2705b85bd8b750d73e72f965244a40e9dcec
David Otterdahl <david.otterdahl@gmail.com>
2019-12-20 15:20:47 +0100

    Add test for S0013 police key

--- spec/site/status_spec.rb
+++ spec/site/status_spec.rb
@@ -31,4 +31,35 @@ RSpec.describe "RSMP site status" do
     end
   end
 
+  it 'S0013 police key' do
+    TestSite.connected do |task,supervisor,site|
+      component = MAIN_COMPONENT
+      status_code = 'S0013'
+      status_name = 'status'
+
+      site.log "Requesting police key", level: :test
+      start_time = Time.now
+      message, response = nil,nil
+      expect do
+        message, response = site.request_status component, [{'sCI'=>status_code,'n'=>status_name}], 180
+      end.not_to raise_error
+
+      expect(response).not_to be_a(RSMP::MessageNotAck), "Message rejected: #{response.attributes['rea']}"
+      expect(response).to be_a(RSMP::StatusResponse)
+
+      delay = Time.now - start_time
+      site.log "Got police key after #{delay}s", level: :test
+
+      expect(response.attributes["cId"]).to eq(component)
+      expect(response.attributes["sS"]).to be_a(Array)
+
+      item = response.attributes["sS"].first
+
+      expect(item["sCI"]).to eq(status_code)
+      expect(item["n"]).to eq(status_name)
+
+      expect(item["s"]).to match(/^[0-2](,[0-2])*$/)
+      expect(item["q"]).to eq('recent')
+    end
+  end
 end
