3d055963713e5c46d81f41cd5f7eadbd6ecee339
David Otterdahl <david.otterdahl@gmail.com>
2020-02-07 15:04:01 +0100

    alarm_spec: Add first test case for alarm

---
+++ spec/site/alarm_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe 'RSMP site alarm' do
+  it 'A0301 detector error (hardware)' do |example|
+    TestSite.log_test_header example
+    TestSite.connected do |task,supervisor,site|
+      component = COMPONENT_CONFIG['signal_group_prefix'] + "001"
+
+      site.log "Waiting for alarm", level: :test
+      start_time = Time.now
+      message, response = nil,nil
+      expect do
+        response = site.wait_for_alarm component: component, aCId: 'A0301',
+          aSp: 'Issue', aS: 'Active', timeout: RSMP_CONFIG['alarm_timeout']
+      end.to_not raise_error, "Did not receive alarm"
+      
+      delay = Time.now - start_time
+      site.log "alarm confirmed after #{delay.to_i}s", level: :test
+    end
+  end
+end
