89faca53077c722e03e4b812421ada4cc8766bc7
Emil Tin <emil@tin.dk>
2020-04-13 23:48:48 +0200

    check configs when starting

--- spec/support/spec_helper.rb
+++ spec/support/spec_helper.rb
@@ -26,19 +26,16 @@ def load_secrets path
 	secrets = YAML.load_file(secrets_path)
 
 	required_keys = ['security_codes']
-	required_keys.each { |key| verify_presence_of_secret secrets, secrets_path, key }
+	required_keys.each do |key|
+		unless secrets[key]
+			puts "The key '#{key}' is missing from #{secrets_path}. Please add it and try again."
+			exit
+		end
+	end
 	secrets
 end
 
-def verify_presence_of_secret secrets, secrets_path, key
-	unless secrets[key]
-		puts "The key '#{key}' is missing from #{secrets_path}. Please add it and try again."
-		exit
-	end
-end
-
-
- def ask_user site, question, accept:''
+def ask_user site, question, accept:''
 	pointing = "\u{1f449}"
 	print "#{pointing} " + question.colorize(:color => :light_magenta) + " "
 	site.log "Asking user for input: #{question}", level: :test
@@ -49,27 +46,51 @@ end
 		site.log "Test skipped by user", level: :test
 		expect(response).to eq(accept), "Test skipped by user"
 	end
- end
-
+end
 
 ASYNC_STDIN = Async::IO::Stream.new( Async::IO::Generic.new($stdin) )
 
-VALIDATOR_CONFIG = YAML.load_file 'config/validator.yaml' rescue {}
+validator_config = YAML.load_file 'config/validator.yaml'
+raise "Error: File config/validator.yaml is missing" unless validator_config
 
-rsmp_config_path = VALIDATOR_CONFIG['rsmp_config_path']
-rsmp_config_path = 'config/ruby.yaml' unless rsmp_config_path
+rsmp_config_path = validator_config['rsmp_config_path'] || 'config/ruby.yaml'
 RSMP_CONFIG = YAML.load_file rsmp_config_path
+puts "Using test config #{rsmp_config_path}"
 
-LOG_CONFIG = YAML.load_file VALIDATOR_CONFIG['log_config_path'] rescue {}
+if validator_config['log_config_path']
+	LOG_CONFIG = YAML.load_file validator_config['log_config_path'] 
+else
+	LOG_CONFIG = {}
+end
 
 SECRETS = load_secrets 'config/secrets.yaml'
 
 #sugar
 SUPERVISOR_CONFIG = RSMP_CONFIG['supervisor'] rescue {}
-SITE_CONFIG = SUPERVISOR_CONFIG['sites'].values.first rescue {}
-COMPONENT_CONFIG = SITE_CONFIG['components'] rescue {}
-MAIN_COMPONENT = COMPONENT_CONFIG['main'].keys.first
-STATUS_RESPONSE_TIMEOUT = SUPERVISOR_CONFIG['status_response_timeout']
+puts "Warning: #{rsmp_config_path} supervisor settings is missing or empty" if SUPERVISOR_CONFIG == {}
 
-puts "Using test config #{VALIDATOR_CONFIG['rsmp_config_path']}"
+SITE_CONFIG = SUPERVISOR_CONFIG['sites'].values.first rescue {}
+puts "Warning: #{rsmp_config_path} sites settings is missing or empty" if SITE_CONFIG == {}
+
+COMPONENT_CONFIG = SITE_CONFIG['components'] rescue {}
+puts "Warning: #{rsmp_config_path} components settings is missing or empty" if COMPONENT_CONFIG == {}
+
+
+MAIN_COMPONENT = COMPONENT_CONFIG['main'].keys.first rescue {}
+puts "Warning: #{rsmp_config_path} main component settings is missing or empty" if MAIN_COMPONENT == {}
+
+
+# check required configs
+required = [
+	'connect_timeout',
+	'ready_timeout',
+	'command_timeout',
+	'status_timeout',
+	'subscribe_timeout',
+	'alarm_timeout',
+	'shutdown_timeout'
+]
+required.each do |key|
+	raise "Config #{key} is missing from #{rsmp_config_path}" unless RSMP_CONFIG[key]
+end
 
