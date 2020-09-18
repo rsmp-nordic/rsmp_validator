0ec731d8d955aa392a1f29205b865e449ec380b6
David Otterdahl <david.otterdahl@gmail.com>
2020-09-18 08:39:08 +0200

    Temporarily disable the alarm test
    We would like to explore the ability to automatically run rspec
    Therefore, we need to disable any tests that require human intervetion

--- spec/site/alarm_spec.rb
+++ spec/site/alarm_spec.rb
@@ -1,20 +1,20 @@
 RSpec.describe 'RSMP site alarm' do
-  it 'A0301 detector error (hardware)' do |example|
-    TestSite.log_test_header example
-    TestSite.connected do |task,supervisor,site|
-      component = COMPONENT_CONFIG['detector_logic'].keys.first
-      if ask_user site, "Please activate detector error on equipment. Press enter when ready or 's' to skip: ".colorize(:color => :light_magenta)
-        site.log "Waiting for alarm", level: :test
-        start_time = Time.now
-        message, response = nil,nil
-        expect do
-          response = site.wait_for_alarm component: component, aCId: 'A0301',
-            aSp: 'Issue', aS: 'Active', timeout: RSMP_CONFIG['alarm_timeout']
-        end.to_not raise_error, "Did not receive alarm"
-        
-        delay = Time.now - start_time
-        site.log "alarm confirmed after #{delay.to_i}s", level: :test
-      end
-    end
-  end
+#  it 'A0301 detector error (hardware)' do |example|
+#    TestSite.log_test_header example
+#    TestSite.connected do |task,supervisor,site|
+#      component = COMPONENT_CONFIG['detector_logic'].keys.first
+#      if ask_user site, "Please activate detector error on equipment. Press enter when ready or 's' to skip: ".colorize(:color => :light_magenta)
+#        site.log "Waiting for alarm", level: :test
+#        start_time = Time.now
+#        message, response = nil,nil
+#        expect do
+#          response = site.wait_for_alarm component: component, aCId: 'A0301',
+#            aSp: 'Issue', aS: 'Active', timeout: RSMP_CONFIG['alarm_timeout']
+#        end.to_not raise_error, "Did not receive alarm"
+#        
+#        delay = Time.now - start_time
+#        site.log "alarm confirmed after #{delay.to_i}s", level: :test
+#      end
+#    end
+#  end
 end
