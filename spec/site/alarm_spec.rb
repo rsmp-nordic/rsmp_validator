b088db7fd33f2c1e70273e5a5a9fb16866415163
Gunnar Rolander <Gunnar.rolander@gmail.com>
2020-11-10 15:55:20 +0100

    Added schema test to alarm A0302

--- spec/site/alarm_spec.rb
+++ spec/site/alarm_spec.rb
@@ -11,12 +11,19 @@ RSpec.describe 'RSMP site alarm' do
         response = site.wait_for_alarm component: component, aCId: 'A0302',
           aSp: 'Issue', aS: 'Active', timeout: RSMP_CONFIG['alarm_timeout']
       end.to_not raise_error, "Did not receive alarm"
-      
-      expect(response.attributes["aTs"].utc).to be_within(1.minute).of Time.now
-        
+
       delay = Time.now - start_time
       site.log "alarm confirmed after #{delay.to_i}s", level: :test
       system("~/deactivate_alarm.sh")
+
+      expect(response.attributes["aTs"].utc).to be_within(1.minute).of Time.now
+      expect(response.attributes['rvs']).to eq([{
+        "n":"detector","v":"1"},
+        {"n":"type","v":"loop"},
+        {"n":"errormode","v":"on"},
+        {"n":"manual","v":"True"},
+        {"n":"logicerror","v":"always_off"}
+      ])
     end
   end
 end
