9baf86b3e9f18270dece375ca2f5dde9dde3f0ba
Emil Tin <emil@tin.dk>
2020-12-22 11:13:26 +0100

    fix unknown status spec, but wait api should be updated

--- spec/site/unknown_status_spec.rb
+++ spec/site/unknown_status_spec.rb
@@ -2,26 +2,24 @@ RSpec.describe "RSMP site status" do
   it 'responds with NotAck to invalid status request code' do |example|
     TestSite.connected do |task,supervisor,site|
       site.log "Requesting non-existing status S0000", level: :test
-      response = site.fetch_status site.task, { component: MAIN_COMPONENT, 
-        status_list: {S0000:[:status]},
-        timeout: SUPERVISOR_CONFIG['command_response_timeout']
-      }
-      expect(response).to be_a(RSMP::MessageNotAck)
-      expect(response.attributes['rea']).not_to be_nil
-      expect(response.attributes['rea']).not_to be('')
+      expect {
+          site.fetch_status site.task, { component: MAIN_COMPONENT, 
+          status_list: {S0000:[:status]},
+          timeout: SUPERVISOR_CONFIG['command_response_timeout']
+        }
+      }.to raise_error(RSMP::MessageRejected)
     end
   end
 
   it 'responds with NotAck to invalid status request name' do |example|
     TestSite.connected do |task,supervisor,site|
       site.log "Requesting non-existing status S0001 name", level: :test
-      response = site.fetch_status site.task, { component: MAIN_COMPONENT, 
-        status_list: {S0000:[:bad]},
-        timeout: SUPERVISOR_CONFIG['command_response_timeout']
-      }
-      expect(response).to be_a(RSMP::MessageNotAck)
-      expect(response.attributes['rea']).not_to be_nil
-      expect(response.attributes['rea']).not_to be('')
+      expect {
+        site.fetch_status site.task, { component: MAIN_COMPONENT, 
+          status_list: {S0000:[:bad]},
+          timeout: SUPERVISOR_CONFIG['command_response_timeout']
+        }
+      }.to raise_error(RSMP::MessageRejected)
     end
   end
 
