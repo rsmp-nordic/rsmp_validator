70fbe6a9ea52bcc28398b115234866ca1fbee006
Gunnar Rolander <Gunnar.rolander@gmail.com>
2021-03-09 16:59:01 +0100

    Added status documentation

--- spec/site/status_spec.rb
+++ spec/site/status_spec.rb
@@ -1,126 +1,250 @@
 RSpec.describe "RSMP site status" do
   include StatusHelpers
-
+  # Verify status S0001 signal group status
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0001 signal group status', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "signal group status",
       { S0001: [:signalgroupstatus, :cyclecounter, :basecyclecounter, :stage] }
   end
 
+  # Verify status S0002 detector logic status
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0002 detector logic status', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "detector logic status",
       { S0002: [:detectorlogicstatus] }
   end
 
+  # Verify status S0003 input status
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0003 input status', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "input status",
       { S0003: [:inputstatus] }
   end
 
+  # Verify status S0004 output status
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0004 output status', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "output status",
       { S0004: [:outputstatus] }
- end
+  end
 
+  # Verify status S0005 traffic controller starting
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0005 traffic controller starting', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "traffic controller starting (true/false)",
       { S0005: [:status] }
   end
 
+  # Verify status S0006 emergency stage
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0006 emergency stage', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "emergency stage status",
       { S0006: [:status,:emergencystage] }
- end
+  end
 
+  # Verify status S0007 controller switched on (dark mode=off)
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0007 controller switched on (dark mode=off)', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "controller switch on (dark mode=off)",
       { S0007: [:status,:intersection] }
   end
 
+  # Verify status S0008 manual control
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0008 manual control', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "manual control status",
       { S0008: [:status,:intersection] }
   end
 
+  # Verify status S0009 fixed time control
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0009 fixed time control', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "fixed time control status",
       { S0009: [:status,:intersection] }
   end
 
+  # Verify status S0010 isolated control
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0010 isolated control', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "isolated control status",
       { S0010: [:status,:intersection] }
   end
 
+  # Verify status S0011 yellow flash
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0011 yellow flash', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "yellow flash status",
       { S0011: [:status,:intersection] }
   end
 
+  # Verify status S0012 all red
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0012 all red', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "all-red status",
       { S0012: [:status,:intersection] }
   end
 
+  # Verify status S0013 police key
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0013 police key', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "police key",
       { S0013: [:status] }
   end
 
+  # Verify status S0014 current time plan
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0014 current time plan', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "current time plan",
       { S0014: [:status] }
   end
 
+  # Verify status S0015 current traffic situation
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0015 current traffic situation', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "current traffic situation",
       { S0015: [:status] }
   end
 
+  # Verify status S0016 number of detector logics
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0016 number of detector logics', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "number of detector logics",
       { S0016: [:number] }
   end
 
+  # Verify status S0017 number of signal groups
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0017 number of signal groups', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "number of signal groups",
       { S0017: [:number] }
   end
 
+  # Verify status S0018 number of time plans
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0018 number of time plans', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "number of time plans",
       { S0018: [:number] }
   end
 
+  # Verify status S0019 number of traffic situations
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0019 number of traffic situations', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "number of traffic situations",
       { S0019: [:number] }
   end
 
+  # Verify status S0020 control mode
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0020 control mode', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "control mode",
       { S0020: [:controlmode,:intersection] }
   end
 
+  # Verify status S0021 manually set detector logic
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0021 manually set detector logic', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "manually set detector logics",
       { S0021: [:detectorlogics] }
   end
 
+  # Verify status S0022 list of time plans
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0022 list of time plans', sxl: '>=1.0.13' do |example|
     request_status_and_confirm "list of time plans",
       { S0022: [:status] }
   end
 
+  # Verify status S0023 command table
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0023 command table', sxl: '>=1.0.13' do |example|
     request_status_and_confirm "command table",
       { S0023: [:status] }
   end
 
+  # Verify status S0024 offset time
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0024 offset time', sxl: '>=1.0.13' do |example|
     request_status_and_confirm "offset time",
       { S0024: [:status] }
   end
 
+  # Verify status S0025 time-of-green/time-of-red
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0025 time-of-green/time-of-red', sxl: '>=1.0.13' do |example|
     request_status_and_confirm "time-of-green/time-of-red",
       { S0025: [
@@ -135,51 +259,101 @@ RSpec.describe "RSMP site status" do
       COMPONENT_CONFIG['signal_group'].keys.first
   end
 
+  # Verify status S0026 week time table
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0026 week time table', sxl: '>=1.0.13'  do |example|
     request_status_and_confirm "week time table",
       { S0026: [:status] }
   end
 
+  # Verify status S0027 time tables
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0027 time tables', sxl: '>=1.0.13'  do |example|
     request_status_and_confirm "command table",
       { S0027: [:status] }
   end
 
+  # Verify status S0028 cycle time
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0028 cycle time', sxl: '>=1.0.13' do |example|
     request_status_and_confirm "cycle time",
       { S0028: [:status] }
   end
 
+  # Verify status S0029 forced input status
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0029 forced input status', sxl: '>=1.0.13' do |example|
     request_status_and_confirm "forced input status",
       { S0029: [:status] }
   end
 
+  # Verify status S0030 forced output status
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0030 forced output status', sxl: '>=1.0.15' do |example|
     request_status_and_confirm "forced output status",
       { S0030: [:status] }
   end
 
+  # Verify status S0031 trigger level sensitivity for loop detector
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0031 trigger level sensitivity for loop detector', sxl: '>=1.0.15' do |example|
     request_status_and_confirm "trigger level sensitivity for loop detector",
       { S0031: [:status] }
   end
 
+  # Verify status S0091 operator logged in/out OP-panel
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0091 operator logged in/out OP-panel', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "operator logged in/out OP-panel",
       { S0091: [:status, :user] }
   end
 
+  # Verify status S0092 operator logged in/out web-interface
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0092 operator logged in/out web-interface', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "operator logged in/out web-interface",
       { S0092: [:status, :user] }
   end
 
+  # Verify status S0095 version of traffic controller
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0095 version of traffic controller', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "version of traffic controller",
       { S0095: [:status] }
   end
 
+  # Verify status 0096 current date and time
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0096 current date and time', sxl: '>=1.0.7'  do |example|
     request_status_and_confirm "current date and time",
       { S0096: [
@@ -192,6 +366,11 @@ RSpec.describe "RSMP site status" do
       ] }
   end
 
+  # Verify status S0097 version of traffic program
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0097 version of traffic program', sxl: '>=1.0.15' do |example|
     request_status_and_confirm "version of traffic program",
       { S0097: [:version,:hash] }
@@ -199,24 +378,44 @@ RSpec.describe "RSMP site status" do
 
   # S0098 missing
 
+  # Verify status S0201 traffic counting: number of vehicles
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0201 traffic counting: number of vehicles', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "traffic counting: number of vehicles",
       { S0201: [:starttime,:vehicles] },
       COMPONENT_CONFIG['detector_logic'].keys.first
   end
 
+  # Verify status S0202 traffic counting: vehicle speed
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0202 traffic counting: vehicle speed', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "traffic counting: vehicle speed",
       { S0202: [:starttime,:speed] },
       COMPONENT_CONFIG['detector_logic'].keys.first
   end
 
+  # Verify status S0203 traffic counting: occupancy
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0203 traffic counting: occupancy', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "traffic counting: occupancy",
       { S0203: [:starttime,:occupancy] },
       COMPONENT_CONFIG['detector_logic'].keys.first
   end
 
+  # Verify status S0204 traffic counting: classification
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0204 traffic counting: classification', sxl: '>=1.0.7' do |example|
     request_status_and_confirm "traffic counting: classification",
       { S0204: [
@@ -234,21 +433,41 @@ RSpec.describe "RSMP site status" do
       COMPONENT_CONFIG['detector_logic'].keys.first
   end
 
+  # Verify status S0205 traffic counting: number of vehicles
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0205 traffic counting: number of vehicles', sxl: '>=1.0.14' do |example|
     request_status_and_confirm "traffic counting: number of vehicles",
       { S0205: [:start,:vehicles] }
   end
 
+  # Verify status S0206 traffic counting: vehicle speed
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0206 traffic counting: vehicle speed', sxl: '>=1.0.14' do |example|
     request_status_and_confirm "traffic counting: vehicle speed",
       { S0206: [:start,:speed] }
   end
 
+  # Verify status S0207 traffic counting: occupancy
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0207 traffic counting: occupancy', sxl: '>=1.0.14' do |example|
     request_status_and_confirm "traffic counting: occupancy",
       { S0207: [:start,:occupancy] }
   end
 
+  # Verify status S0208 traffic counting: classification
+  #
+  # 1: Verify connection
+  # 2: Request status
+  # 3: Confirme status response
   it 'S0208 traffic counting: classification', sxl: '>=1.0.14' do |example|
     request_status_and_confirm "traffic counting: classification",
       { S0208: [
