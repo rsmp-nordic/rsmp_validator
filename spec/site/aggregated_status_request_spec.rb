3b979016767324dd54f636b219918fa88fe37776
Gunnar Rolander <Gunnar.rolander@gmail.com>
2021-03-12 13:42:48 +0100

    Added documentation to connect and aggregated status spec

--- spec/site/aggregated_status_request_spec.rb
+++ spec/site/aggregated_status_request_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe "RSMP aggregated status" do
   include CommandHelpers
 
+  # Verify that the controller responds to an aggregated status request.
+  #
+  # 1. Given the site is connected
+  # 2. Request aggregated status 
+  # 3. Expect aggregated status response before timeout
   it 'request aggregated status', rsmp: '>=3.1.5' do |example|
     TestSite.connected do |task,supervisor,site|
       prepare task, site
