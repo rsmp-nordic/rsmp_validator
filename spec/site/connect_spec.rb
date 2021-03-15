8a59b89937e18d96c62570df97260b1475b1c724
Gunnar Rolander <Gunnar.rolander@gmail.com>
2021-03-15 13:03:08 +0100

    Added changes suggested in PR

--- spec/site/connect_spec.rb
+++ spec/site/connect_spec.rb
@@ -1,14 +1,14 @@
 RSpec.describe "RSMP site connection" do
 
-  def check_connection_sequence version, expected
+  def check_connection_sequence version, Expected
     TestSite.isolated(
       'rsmp_versions' => [version],
-      'collect' => expected.size
+      'collect' => Expected.size
     ) do |task,supervisor,site|
       site.collector.collect task, timeout: RSMP_CONFIG['ready_timeout']
       got = site.collector.messages.map { |message| [message.direction.to_s, message.type] }
-      expect(got).to eq(expected)
-      expect(site.ready?).to be true
+      Expect(got).to eq(Expected)
+      Expect(site.ready?).to be true
     end
   end
 
@@ -51,54 +51,53 @@ RSpec.describe "RSMP site connection" do
     end
   end
 
-# Verify that the handshake sequence upon connection is correct for 3.1.1
-#
-# 1. Given the site is new, connected and version 3.1.1
-# 2. Send and receive handshake messages
-# 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.1
-# 4. Expect the site to be ready
+  # Verify the connection sequence when using rsmp core 3.1.1
+  #
+  # 1. Given the site is connected and using core 3.1.1
+  # 2. Send and receive handshake messages
+  # 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.1
+  # 4. Expect the connection sequence to be complete
   it 'exchanges correct connection sequence of rsmp version 3.1.1', rsmp: '3.1.1' do |example|
     check_sequence '3.1.1'
   end
 
-# Verify that the handshake sequence upon connection is correct for 3.1.2
-#
-# 1. Given the site is new, connected and version 3.1.2
-# 2. Send and receive handshake messages
-# 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.2
-# 4. Expect the site to be ready
+  # Verify the connection sequence when using rsmp core 3.1.2
+  #
+  # 1. Given the site is connected and using core 3.1.2
+  # 2. Send and receive handshake messages
+  # 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.2
+  # 4. Expect the connection sequence to be complete
   it 'exchanges correct connection sequence of rsmp version 3.1.2', rsmp: '3.1.2' do |example|
     check_sequence '3.1.2'
   end
 
-# Verify that the handshake sequence upon connection is correct for 3.1.3
-#
-# 1. Given the site is new, connected and version 3.1.3
-# 2. Send and receive handshake messages
-# 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.3
-# 4. Expect the site to be ready
+  # Verify the connection sequence when using rsmp core 3.1.3
+  #
+  # 1. Given the site is connected and using core 3.1.3
+  # 2. Send and receive handshake messages
+  # 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.3
+  # 4. Expect the connection sequence to be complete
   it 'exchanges correct connection sequence of rsmp version 3.1.3', rsmp: '3.1.3' do |example|
     check_sequence '3.1.3'
   end
 
-# Verify that the handshake sequence upon connection is correct for 3.1.4
-#
-# 1. Given the site is new, connected and version 3.1.4
-# 2. Send and receive handshake messages
-# 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.4
-# 4. Expect the site to be ready
+  # Verify the connection sequence when using rsmp core 3.1.4
+  #
+  # 1. Given the site is connected and using core 3.1.4
+  # 2. Send and receive handshake messages
+  # 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.4
+  # 4. Expect the connection sequence to be complete
   it 'exchanges correct connection sequence of rsmp version 3.1.4', rsmp: '3.1.4' do |example|
     check_sequence '3.1.4'
   end
 
-# Verify that the handshake sequence upon connection is correct for 3.1.5
-#
-# 1. Given the site is new, connected and version 3.1.5
-# 2. Send and receive handshake messages
-# 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.5
-# 4. Expect the site to be ready
+  # Verify the connection sequence when using rsmp core 3.1.5
+  #
+  # 1. Given the site is connected and using core 3.1.5
+  # 2. Send and receive handshake messages
+  # 3. Expect the handshake messages to be in the specified sequence corresponding to version 3.1.5
+  # 4. Expect the connection sequence to be complete
   it 'exchanges correct connection sequence of rsmp version 3.1.5', rsmp: '3.1.5' do |example|
     check_sequence '3.1.5'
   end
-
 end
