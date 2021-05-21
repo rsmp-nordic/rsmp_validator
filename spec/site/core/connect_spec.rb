02a79e56ee46afa31e338c1e24fcc79c43825085
Emil Tin <emil@tin.dk>
2021-05-21 15:08:45 +0200

    update connect spec to reflect spec

--- spec/site/core/connect_spec.rb
+++ spec/site/core/connect_spec.rb
@@ -1,32 +1,50 @@
 RSpec.describe "Traffic Light Controller" do
 
-  def check_connection_sequence version, expected
+  def get_connection_message core_version, length
+    got = nil
     TestSite.isolated(
-      'rsmp_versions' => [version],
-      'collect' => expected.size
+      'rsmp_versions' => [core_version],
+      'collect' => length
     ) do |task,supervisor,site|
       site.collector.collect task, timeout: RSMP_CONFIG['ready_timeout']
-      got = site.collector.messages.map { |message| [message.direction.to_s, message.type] }
-      expect(got).to eq(expected)
       expect(site.ready?).to be true
+      got = site.collector.messages.map { |message| [message.direction.to_s, message.type] }
     end
+    got
   end
 
-  def check_sequence_from_3_1_1 version
-    check_connection_sequence version, [
+  def check_sequence_3_1_1 core_version
+    # in earlier core version, both sides sends a Version
+    # message simulatenously. we therefore cannot expect
+    # a specific sequence
+    # but we can expect a set of messages
+
+    expected_version_messages = [
       ['in','Version'],
       ['out','MessageAck'],
       ['out','Version'],
       ['in','MessageAck'],
+    ]
+    expected_watchdog_messages = [
       ['in','Watchdog'],
       ['out','MessageAck'],
       ['out','Watchdog'],
       ['in','MessageAck']
     ]
+
+    length = expected_version_messages.length +
+             expected_watchdog_messages.length
+
+    got = get_connection_message core_version, length
+    got_version_messages = got[0..3]
+    got_watchdog_messages = got[4..7]
+
+    expect(got_version_messages).to include(*expected_version_messages)
+    expect(expected_watchdog_messages).to include(*got_watchdog_messages)
   end
 
-  def check_sequence_from_3_1_3 version
-    check_connection_sequence version, [
+  def check_sequence_3_1_4 version
+    expected = [
       ['in','Version'],
       ['out','MessageAck'],
       ['out','Version'],
@@ -38,14 +56,16 @@ RSpec.describe "Traffic Light Controller" do
       ['in','AggregatedStatus'],
       ['out','MessageAck']
     ]
+    got = get_connection_message version, expected.length
+    expect(got).to eq(expected)
   end
 
   def check_sequence version
     case version
-    when '3.1.1', '3.1.2'
-      check_sequence_from_3_1_1 version
-    when '3.1.3', '3.1.4', '3.1.5'
-      check_sequence_from_3_1_3 version
+    when '3.1.1', '3.1.2', '3.1.3'
+      check_sequence_3_1_1 version
+    when '3.1.4', '3.1.5'
+      check_sequence_3_1_4 version
     else
       raise "Unkown rsmp version #{version}"
     end
