7069f1d67be5001b057264c229f9cc781bcf141a
Emil Tin <emil@tin.dk>
2021-07-01 12:11:01 +0200

    improve core spec grouping

--- spec/supervisor/aggregated_status_spec.rb
+++ spec/supervisor/aggregated_status_spec.rb
@@ -1,13 +1,15 @@
-RSpec.describe 'RSMP supervisor' do
-  it 'receives aggretated status' do
-    Validator::Supervisor.connected do |task,site,supervisor_proxy|
-      component = site.find_component Validator.config['main_component']
+RSpec.describe 'Supervisor' do
+  describe 'Aggregated Status' do
+    it 'receives aggregated status' do
+      Validator::Supervisor.connected do |task,site,supervisor_proxy|
+        component = site.find_component Validator.config['main_component']
 
-      # setting ':collect' will cause set_aggregated_status() to wait for the
-      # outgoing aggregated status is acknowledged
-      component.set_aggregated_status :high_priority_alarm, collect: {
-        timeout: Validator.config['timeouts']['acknowledgement']
-      }
+        # setting ':collect' will cause set_aggregated_status() to wait for the
+        # outgoing aggregated status is acknowledged
+        component.set_aggregated_status :high_priority_alarm, collect: {
+          timeout: Validator.config['timeouts']['acknowledgement']
+        }
+      end
     end
   end
 end
