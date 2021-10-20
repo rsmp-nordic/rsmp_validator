f313e7161bd59ca86b5864a7be66d346dad53ad5
David Otterdahl <david.otterdahl@gmail.com>
2021-10-20 15:44:48 +0200

    alarm_spec: Added new test case for testing

--- spec/site/tlc/alarm_spec.rb
+++ spec/site/tlc/alarm_spec.rb
@@ -139,5 +139,47 @@ RSpec.describe 'Site::Traffic Light Controller' do
         system(Validator.config['scripts']['deactivate_alarm'])
       end
     end
+    
+    # Validate that alarm triggered during a RSMP disconnect is buffered
+    # and send once the RSMP connection is reestablished.
+    #
+    # 1. Given the site is disconnected
+    # 2. And we trigger an alarm using an external script
+    # 3. When the site reconnects
+    # 4. Then we should received an alarm
+
+    it 'is buffered during disconnect', :script, sxl: '>=1.0.7' do |example|
+      require_scripts
+      component = Validator.config['components']['detector_logic'].keys.first
+      Validator::Site.isolated do |task,supervisor,site|
+      end
+      # Activate alarm
+      system(Validator.config['scripts']['activate_alarm'])
+
+      Validator::Site.isolated do |task,supervisor,site|
+        site = site
+        log_confirmation "Waiting for alarm" do
+          message, response = nil,nil
+          expect do
+            response = site.wait_for_alarm task, component: component, aCId: 'A0302',
+              aSp: 'Issue', aS: 'Active', timeout: Validator.config['timeouts']['alarm']
+          end.to_not raise_error, "Did not receive alarm"
+
+        end
+        system(Validator.config['scripts']['deactivate_alarm'])
+
+        alarm_time = Time.parse(response[:message].attributes["aTs"])
+        expect(alarm_time).to be_within(1.minute).of Time.now.utc
+        expect(response[:message].attributes['rvs']).to eq([{
+          "n":"detector","v":"1"},
+          {"n":"type","v":"loop"},
+          {"n":"errormode","v":"on"},
+          {"n":"manual","v":"True"},
+          {"n":"logicerror","v":"always_off"}
+        ])
+      ensure
+        system(Validator.config['scripts']['deactivate_alarm'])
+      end
+    end
   end
 end
