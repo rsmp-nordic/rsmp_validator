810358fee85cd63f597d85b0b165e3c754ae60c3
Emil Tin <emil@tin.dk>
2023-07-14 11:39:04 +0200

    update invalid command test, expect age=undefined when cId is unknown

--- spec/site/tlc/invalid_command_spec.rb
+++ spec/site/tlc/invalid_command_spec.rb
@@ -3,8 +3,8 @@ RSpec.describe 'Site::Traffic Light Controller' do
 
   context 'receiving an command with an unknown component id' do
 
-    # Verify that site reponds with NotAck when receiving a command
-    # request with an unknown component id
+    # Verify that site reponds with age=undefined when receiving
+    # a command with an unknown component id
     #
     # 1. Given the site is connected
     # 2. When we send a command with an unknown component id
@@ -15,12 +15,13 @@ RSpec.describe 'Site::Traffic Light Controller' do
         log "Sending M0001"
         command_list = build_command_list :M0001, :setValue, {
           securityCode: Validator.config['secrets']['security_codes'][2],
+          status: 'NormalControl',
           timeout: 0,
-          intersection: 0,
-          status: 'NormalControl'
+          intersection: 0
         }
         result = site.send_command(
-          'bad', command_list,
+          'bad',
+          command_list,
           collect: { timeout: Validator.config['timeouts']['command_response'] },
           validate: false     # disable validation of outgoing message
         )
@@ -39,7 +40,7 @@ RSpec.describe 'Site::Traffic Light Controller' do
     end
   end
 
-  context 'receiving an unknown command request code id' do
+  context 'receiving an unknown command code id' do
 
     # Verify that site returns NotAck when receiving an unknown command
     #
@@ -50,11 +51,8 @@ RSpec.describe 'Site::Traffic Light Controller' do
     it 'returns NotAck' do |example|
       Validator::Site.connected do |task,supervisor,site|
         log "Sending non-existing command M0000"
-        command_list = build_command_list :M0000, :wrong, {
-          status: 'wrong'
-        }
+        command_list = build_command_list :M0000, :bad, {}
         result = site.send_command Validator.config['main_component'], command_list,
-          securityCode: Validator.config['secrets']['security_codes'][2],
           collect: { timeout: Validator.config['timeouts']['command_response'] },
           validate: false     # disable validation of outgoing message
         collector = result[:collector]
@@ -65,20 +63,20 @@ RSpec.describe 'Site::Traffic Light Controller' do
     end
   end
 
-  context 'receiving a command request with a missing attribute' do
+  context 'receiving a command with a missing attribute' do
 
     # Verify that site returns NotAck when receiving a command
-    # request with a missing command attribute
+    # with a mising command attribute
     #
     # 1. Given the site is connected
-    # 2. When we send an M0001 command with the 'status' attribute missing
+    # 2. When we send an M0001 command with 'status' missing
     # 3. Then the site return NotAck
 
     it 'returns NotAck' do |example|
       Validator::Site.connected do |task,supervisor,site|
-        log "Sending M0001 with empty 'status' attribute"
+        log "Sending M0001 with 'status' attribute missing"
         command_list = build_command_list :M0001, :setValue, {
-            securityCode: Validator.config['secrets']['security_codes'][2],
+            securityCode: '1111',
             intersection: '0',
             timeout: '0'
             # intentionally not setting 'status'
@@ -94,7 +92,7 @@ RSpec.describe 'Site::Traffic Light Controller' do
     end
   end
 
-  context 'receiving a command request with a bad command name' do
+  context 'receiving a command with a bad command name n' do
 
     # Verify that site returns NotAck when receiving a command
     # with an unknown command name
@@ -107,11 +105,10 @@ RSpec.describe 'Site::Traffic Light Controller' do
       Validator::Site.connected do |task,supervisor,site|
         log "Sending M0001"
         # for M0001, cO should be :setValue, here we use the incorrect :bad
-        command_list = build_command_list :M0001, :wrong, {
-            securityCode: Validator.config['secrets']['security_codes'][2],
+        command_list = build_command_list :M0001, :bad, {
+            securityCode: '1111',
             intersection: '0',
-            timeout: '0',
-            status: 'NormalControl'
+            timeout: '0'
         }
         result = site.send_command Validator.config['main_component'], command_list,
           collect: { timeout: Validator.config['timeouts']['command_response'] },
