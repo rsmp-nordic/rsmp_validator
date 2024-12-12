97f4455fc85781e83552f4b96c64349f977a557f
Emil Tin <emil@tin.dk>
2024-12-12 14:03:49 +0100

    fix programming herlp if not configured

--- spec/support/programming_helpers.rb
+++ spec/support/programming_helpers.rb
@@ -1,6 +1,9 @@
 module Validator::ProgrammingHelpers
 	def find_alarm_programming alarm_code_id
-		action = Validator.config.dig('inputs','programming').find do |input,options|
+		action = Validator.config.dig('inputs','programming')
+		skip "Alarm #{alarm_code_id} is not configured" unless action
+
+		action = action.find do |input,options|
 		  options['raise_alarm'] == alarm_code_id
 		end
 		skip "Alarm #{alarm_code_id} is not configured" unless action
