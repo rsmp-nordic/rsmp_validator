76ecb6b759ddc30dbaa4eca0b8bf6fe02713e818
Emil Tin <zf0f@tmf.kk.dk>
2025-01-03 13:56:00 +0100

    cleanup and streamline test configs

--- spec/support/programming_helpers.rb
+++ spec/support/programming_helpers.rb
@@ -1,16 +1,7 @@
 module Validator::ProgrammingHelpers
 	def find_alarm_programming alarm_code_id
-		action = Validator.config.dig('inputs','programming')
-		skip "Alarm #{alarm_code_id} is not configured" unless action
-
-		action = action.find do |input,options|
-		  options['raise_alarm'] == alarm_code_id
-		end
-		skip "Alarm #{alarm_code_id} is not configured" unless action
-
-		input = action.first
-		component_id = action.last['component']
-
-		return input, component_id
+		action = Validator.config.dig('alarm_triggers',alarm_code_id)
+		skip "Alarm trigger #{alarm_code_id} not configured" unless action
+		return action["input"], action["component"]
 	end
 end
