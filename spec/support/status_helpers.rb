fe7780da47b104342c5c45aa6b64943e565ffdd1
Emil Tin <emil@tin.dk>
2025-01-31 10:12:47 +0100

    dont' use config interval/status_update

--- spec/support/status_helpers.rb
+++ spec/support/status_helpers.rb
@@ -48,7 +48,7 @@ module Validator::StatusHelpers
   end
 
   def wait_for_status parent_task, description, status_list,
-      update_rate: Validator.get_config('intervals','status_update', assume: 0),
+      update_rate: 0,
       timeout: Validator.get_config('timeouts','command')
     log "Wait for #{description}"
     subscribe_list = convert_status_list(status_list).map { |item| item.merge 'uRt'=>update_rate.to_s }
@@ -69,7 +69,6 @@ module Validator::StatusHelpers
     wait_for_status(@task,
       "Wait for all groups to go to yellow flash",
       [{'sCI'=>'S0001','n'=>'signalgroupstatus','s'=>regex}],
-      update_rate: 0,
       timeout: timeout
     )
   end
